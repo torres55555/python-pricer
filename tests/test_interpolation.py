b6bf46c0d1e50dbc45d7a4504555f4490a991a3d
option-pricer-cmf2021 <93095579+option-pricer-cmf2021@users.noreply.github.com>
2021-10-24 20:51:14 +0100

    Update test_interpolation.py

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,5 +1,5 @@
 from src.interpolation import Interpolator
 
-def test_correct_work(self):
+def test_correct_work():
     i = Interpolator()
     assert i.interpolate(x_list=[0, 1, 2, 3, 5], y_list=[0, 1, 4, 9, 25], z=2.5) == 6.5
